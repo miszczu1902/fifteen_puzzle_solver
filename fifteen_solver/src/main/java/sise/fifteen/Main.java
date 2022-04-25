ed1d49cb1e3c9bc101fc7e782837bd186f635173
Bartosz Miszczak <85065893+miszczu1902@users.noreply.github.com>
2022-04-25 13:47:48 +0200

    Dodanie czytania planszy

--- fifteen_solver/src/main/java/sise/fifteen/Main.java
+++ fifteen_solver/src/main/java/sise/fifteen/Main.java
@@ -1,7 +1,14 @@
 package sise.fifteen;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main {
     public static void main(String[] args) {
+        Path currentDir = Paths.get("");
+        System.out.println(currentDir.toAbsolutePath());
+        String str = IOFileOperations.readFromFile(currentDir.toAbsolutePath() + "/boards/4x4_01_00001.txt");
+        System.out.println(str);
         System.out.println("Hello world!");
     }
 }
